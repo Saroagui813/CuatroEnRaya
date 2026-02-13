cff409a05ce2ab08daa253bbfd26427c885f7a6a
Saroagui813 <saroagui8132006@gmail.com>
2026-02-13 14:40:25 +0100

    He hecho Ficha Casilla Jugador Excepcion y Tablero empezado

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -1,4 +1,7 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Tablero {
+    public static final int FILAS = 6;
+    public static final int COLUMNAS = 7;
+    public static final int FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS = 4;
 }
