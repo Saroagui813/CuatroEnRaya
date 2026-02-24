8095724fa426598d3e97d03c86fb733733b7f022
Saroagui813 <saroagui8132006@gmail.com>
2026-02-24 14:39:24 +0100

    main hecho

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/Main.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/Main.java
@@ -1,13 +1,17 @@
 package org.iesalandalus.programacion.cuatroenraya;
 
 import org.iesalandalus.programacion.cuatroenraya.modelo.Consola;
+import org.iesalandalus.programacion.cuatroenraya.modelo.CuatroEnRaya;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Ficha;
 import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
 
 public class Main {
 	
 	public static void main(String[] args) {
         Jugador jugador1 = Consola.leerJugador();
-        Jugador jugador2 = Consola.leerJugador();
+        Jugador jugador2 = Consola.leerJugador(jugador1.colorFichas() == Ficha.AZUL ? Ficha.VERDE : Ficha.AZUL);
+        CuatroEnRaya cuatroEnRaya = new CuatroEnRaya(jugador1, jugador1);
+        cuatroEnRaya.jugar();
 	}
 	
 }
